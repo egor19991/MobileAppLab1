8538c96517e39cec01d05690e825c69f34124804
egorkrasnov1999 <egorkrasnov1999@yandex.ru>
2021-11-30 14:27:43 +0700

    Добавленно поле для вывода даты

--- app/src/main/java/com/ek/lab1/MainActivity.java
+++ app/src/main/java/com/ek/lab1/MainActivity.java
@@ -2,13 +2,25 @@ package com.ek.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.icu.text.SimpleDateFormat;
+import android.widget.TextView;
 import android.os.Bundle;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        String formattedDate = df.format(Calendar.getInstance().getTime());
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Date: "+formattedDate);
+
     }
 }
